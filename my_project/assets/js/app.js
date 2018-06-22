d9eb68fff1c2c138177975b92f004b9727fd91b7
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-06-22 11:55:28 +0300

    added yarn

---
+++ my_project/assets/js/app.js
@@ -0,0 +1,6 @@
+import {another_file_test} from './functions';
+var f = function () {
+    console.log("hey hey");
+};
+another_file_test();
+f();
---
+++ my_project/assets/js/functions.js
@@ -0,0 +1,3 @@
+export function another_file_test() {
+    console.log("this comes from another file!");
+}
