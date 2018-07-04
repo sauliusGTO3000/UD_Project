6f31a5ae819c7718e1bdfdca48220a05c088c7fb
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-07-04 13:07:54 +0300

    v0.5.1 added select2 tags for front-end

--- my_project/assets/js/app.js
+++ my_project/assets/js/app.js
@@ -1,6 +1,6 @@
-import {another_file_test} from './functions';
-var f = function () {
-    console.log("hey hey");
-};
-another_file_test();
-f();
+import 'select2';
+
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2({tags:true});
+});
