3c4bb8c13494426befcf803762cf8029d8af9ab3
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-07-30 01:49:23 +0300

    v08.3 author interface updates

--- my_project/src/Form/HashtagType.php
+++ my_project/src/Form/HashtagType.php
@@ -12,7 +12,7 @@ class HashtagType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('hastagName')
+            ->add('hashtagName')
             ->add('readCount')
         ;
     }
