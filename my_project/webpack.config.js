2f3de57cc8923b2f96099d69db6eaf45f8690463
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-07-30 17:39:01 +0300

    responsive navbar v9

--- my_project/webpack.config.js
+++ my_project/webpack.config.js
@@ -27,6 +27,7 @@ Encore
     .addStyleEntry('css/postEdit', './assets/sass/postEdit.scss')
     .addStyleEntry('css/autoriausKampelis', './assets/sass/autoriausKampelis.scss')
     .addStyleEntry('css/responsive_iphone5ss', './assets/sass/responsive_iphone5se.scss')
+    .addStyleEntry('css/responsiveIphone5', './assets/sass/responsiveIphone5.scss')
 
     // uncomment if you use Sass/SCSS files
     .enableSassLoader()
